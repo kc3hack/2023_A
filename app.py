2be4b90fb89e0004882800bfba71d88d754ca66d
herring101 <herring101@outlook.jp>
2023-02-13 13:09:39 +0900

    #8 lineapiもsecretsに追加

--- app.py
+++ app.py
@@ -28,8 +28,9 @@ from linebot.models import (MessageEvent, TextMessage, TextSendMessage,)
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi('v6JcBNiQFQmHVK7qqXb87rcsVmM8QFXQqEDouXT0wWOt+8lAcaOJ4ieOdlUcHmV+qXfvETZM84p/cC8aiU+0B1/iJDkdqNJm58awIYI0hcjpFejLNIjBxBOn68QyLYGVamSSCSGFf7hi4c6KAKJA0AdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('6bd4bc0e7b73f5166b81e493b5bd5443')
+# secrets/line_channel_access_token.txtにアクセストークンを保存して、それを読み込む
+line_bot_api = LineBotApi(open("secrets/line_channel_access_token.txt").read().strip())
+handler = WebhookHandler(open("secrets/line_channel_secret.txt").read().strip())
 
 @app.route("/")
 def test():
